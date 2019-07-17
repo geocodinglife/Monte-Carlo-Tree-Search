dca6f7dbaf7d819f7f5507254a4f8f2ac4fcd592
ricardo alarcon <geocodinglife@gmail.com>
2019-07-17 08:18:24 -0400

    logic of the search

---
+++ monte-carlo.js
@@ -0,0 +1,15 @@
+// Class representig the Monte Carlo Search  Tree
+class MonteCarlo {
+  //From given state, repeatedly run MCTS to build statistics.
+  runSearch(state, timeout) {
+    // TODO
+  }
+
+  // Get the best move from available statistics.
+  bestPlay(state) {
+    // TODO
+    // return play
+  }
+}
+
+module.exports = MonteCarlo
